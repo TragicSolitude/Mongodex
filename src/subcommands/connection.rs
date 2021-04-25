d2771c6837eb136313bdfd62df8f5ec6f0441ea4
Noah Shuart <shuart.noah.s@gmail.com>
2021-04-25 15:49:06 -0600

    chore: cleanup implementation of connection subcommand

--- src/subcommands/connection.rs
+++ src/subcommands/connection.rs
@@ -4,55 +4,35 @@ use crate::ConnectionRepository;
 use crate::connection::Server;
 use anyhow::Context;
 
-async fn list(connections: &mut ConnectionRepository) -> Result<()> {
-    let server_list = connections.list_connections().await?;
-    println!("{}", server_list);
-
-    Ok(())
-}
-
-async fn add<'a, 'b>(connections: &'a mut ConnectionRepository, name: &'b str) -> Result<()> {
-    let info = Server::prompt_details(name)?;
-    connections.add_connection(&info).await?;
-
-    Ok(())
-}
-
-async fn remove<'a, 'b>(connections: &'a mut ConnectionRepository, name: &'b str) -> Result<()> {
-    connections.remove_connection(name).await?;
-
-    Ok(())
-}
-
-async fn edit<'a, 'b>(connections: &'a mut ConnectionRepository, name: &'b str) -> Result<()> {
-    let mut connection = connections.get_connection(name).await?;
-    connection.prompt_update_details()?;
-    connections.replace_connection(&connection).await?;
-
-    Ok(())
-}
-
 pub async fn run<'a, 'b>(connections: &'a mut ConnectionRepository, args: &'b ArgMatches<'b>) -> Result<()> {
     match args.subcommand() {
-        ("list", Some(_args)) => list(connections).await,
-        ("add", Some(args)) => {
-            let name = args.value_of("name")
+        ("list", Some(_subargs)) => {
+            let server_list = connections.list_connections().await?;
+            println!("{}", server_list);
+        },
+        ("add", Some(subargs)) => {
+            let name = subargs.value_of("name")
                 .with_context(|| "Name argument not provided")?;
 
-            add(connections, name).await
+            let info = Server::prompt_details(name)?;
+            connections.add_connection(&info).await?;
         },
-        ("remove", Some(args)) => {
-            let name = args.value_of("name")
+        ("remove", Some(subargs)) => {
+            let name = subargs.value_of("name")
                 .with_context(|| "Name argument not provided")?;
 
-            remove(connections, name).await
+            connections.remove_connection(name).await?;
         },
-        ("edit", Some(args)) => {
-            let name = args.value_of("name")
+        ("edit", Some(subargs)) => {
+            let name = subargs.value_of("name")
                 .with_context(|| "Name argument not provided")?;
 
-            edit(connections, name).await
+            let mut connection = connections.get_connection(name).await?;
+            connection.prompt_update_details()?;
+            connections.replace_connection(&connection).await?;
         },
-        _ => Ok(())
-    }
+        _ => {}
+    };
+
+    Ok(())
 }
